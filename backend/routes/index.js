0480987228c9ffec31525cf6e201049fb2c08e4b
gfdsa1708 <52555633+gfdsa1708@users.noreply.github.com>
2021-01-27 02:48:45 +0900

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data.json');
+const mysql      = require('mysql');
+const dbconfig   = require('../config/database.js');
+const connection = mysql.createConnection(dbconfig);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send(data);
+});
+
+router.get('/actor', function(req, res, next) {
+  connection.query('SELECT actor_id as actorId, first_name as firstName, last_name as lastName, last_update as lastUpdate, birth_date as birthDate FROM ACTOR', (error, data) => {
+    if (error) throw error;
+    res.send(data);
+  });
+});
+
+module.exports = router;
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  created () {
+    this.$http.get('/api/actor')
+    .then((res) => {
+      console.log(res);
+      }
+        ,(err) => {console.log(err)})
+    .catch((err) => {console.log(err)});
+  },
+}
+</script>
