d4e8b81e3b928f3fd6f5f2123b4269a0b16c45cd
gfdsa1708 <gfdsa1708@naver.com>
2021-01-27 13:39:13 +0900

    vuetify install

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,17 @@
-module.exports = { 
-    devServer: {
-      proxy: { 
-        '/api': { 
-          target: 'http://localhost:3000',
-          changeOrigin: true, 
-          pathRewrite: { 
-            '^/api': ''
-          } 
-        } 
-      } 
-    },
-    outputDir: '../backend/public'
-  }
+module.exports = {
+  "devServer": {
+    "proxy": {
+      "/api": {
+        "target": "http://localhost:3000",
+        "changeOrigin": true,
+        "pathRewrite": {
+          "^/api": ""
+        }
+      }
+    }
+  },
+  "outputDir": "../backend/public",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
